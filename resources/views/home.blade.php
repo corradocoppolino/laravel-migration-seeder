c7ffafc75b311671fa55174eac1f0359bfaa17ff
corradocoppo <corradocoppolino@gmail.com>
2021-06-25 00:39:58 +0200

    finito v.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,12 @@
     <section>
     
         <div class="jumbotron text-center">
+            
             <h1>BOOKLEAN.COM</h1> 
             <p>Il tuo compagno di viaggio</p>
-            <img src="https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg" alt=""></div>
-    
+            <img src="https://technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg" alt="">
+        
+        </div>
     </section>
 
 @endsection
