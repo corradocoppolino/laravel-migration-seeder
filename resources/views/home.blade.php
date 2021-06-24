1f146587fcf80bf567ef684fb2c54174becfd10d
corradocoppo <corradocoppolino@gmail.com>
2021-06-24 15:46:07 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <section class="container mt-5">
+    
+        <h1>Viaggi</h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+    
+    </section>
+
+@endsection
